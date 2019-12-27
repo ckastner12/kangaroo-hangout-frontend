46c6984257125d912f9f8787b20a331896f30a4f
ckastner12 <github email address>
2019-12-27 11:01:59 -0800

    Working on google maps search form

--- src/containers/GoogleMaps.js
+++ src/containers/GoogleMaps.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {GoogleMap as Map} from 'google-map-react';
+import GoogleMap from 'google-map-react';
+import SearchForm from '../presentational/SearchForm'
 
 const GoogleMaps = props => {
     return (
         <div>
             <h3>I'll display the google maps tool</h3>
-            <Map bootstrapURLKeys={{
+            <SearchForm />
+            <GoogleMap bootstrapURLKeys={{
                 key: "AIzaSyBCHWjw3rHXuSkQDOz2wF7u6nbx9BI3zqk",
                 language: 'en',
                 region: 'en'}}
--- src/presentational/SearchForm.js
+++ src/presentational/SearchForm.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import {Button, Form, Checkbox} from 
+import {Button, Form, Select} from 'semantic-ui-react'
 
+const SelectOptions = (props)
 
-const SearchForm = props => {
+const SearchForm = (props) => {
     return (
-        <Form>
+        <Form className="search-form">
             <Form.Field>
                 <label>Search</label>
                 <input placeholder="Search" />
             </Form.Field>
-            <Form.Field label='Type' control='select'>
-                <option value='male'>Male</option>
-                <option value='female'>Female</option>
-            </Form.Field>
-
+            <Select 
         </Form>
     )
 }
+
+export default SearchForm
