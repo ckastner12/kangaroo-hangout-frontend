61f893592d84053604a2690cf3432fb16ddab956
ckastner12 <github email address>
2019-12-28 15:54:30 -0800

    Implemented kangaroo loader

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react'
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <img className="gif" src="https://66.media.tumblr.com/cdace9d2fb94029e0c028fd6ed6fe635/tumblr_nkfx7qq6ct1serey3o1_1280.gifv" /><br></br>
+            <b>Loading...</b>
+        </div>
+    )
+}
+
+export default Loader
--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import LoginModal from '../components/LoginModal'
+import Loader from '../components/Loader'
 
 export default class LandingContainer extends React.Component {
 
     render() {
         return (
-            <LoginModal />
+            <Loader />
         )
     }
 }
