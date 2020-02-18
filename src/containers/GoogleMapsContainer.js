d3091b83e58ff37b919fe5682ad41997e3d1853c
Christian Kastner <christianmkastner@gmail.com>
2020-02-18 14:57:35 -0800

    google maps displaying map correctly

--- src/containers/GoogleMapsContainer.js
+++ src/containers/GoogleMapsContainer.js
@@ -4,6 +4,8 @@ import SearchForm from '../presentational/SearchForm'
 import Loader from "../components/Loader"
 import { Divider } from 'semantic-ui-react'
 
+console.log(process.env.REACT_APP_GOOGLE_API_KEY)
+
 const GoogleMapsContainer = props => {
     return (
         <div className="search-activities search-bar">
@@ -16,7 +18,7 @@ const GoogleMapsContainer = props => {
             <Divider />
             <div style={{width: '400px', height: '400px'}} >
                 <MapContainer 
-                    googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
                     loadingElement={<div style={{height: '100%'}} ><Loader /></div>}
                     containerElement={<div style={{ height: `400px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
