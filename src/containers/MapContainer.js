26ca6b1f64641eda0f6514b86a6206fcb3ae024a
Christian Kastner <christianmkastner@gmail.com>
2020-02-19 17:38:51 -0800

    Functional inspect

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps"
 
 const Map = props => {
@@ -7,7 +7,7 @@ const Map = props => {
         props.markers.map(place => {
             console.log(place)
             const { lat, lng } = place.geometry.location
-            return (<Marker key={place.id} position={{lat: lat, lng: lng}} onClick={() => props.handleAdd(place)}/>)
+            return (<Marker key={place.id} position={{lat: lat, lng: lng}} onClick={() => props.handleSelectPlace(place)}/>)
             })
     )
 
@@ -15,14 +15,14 @@ const Map = props => {
     return (
         <GoogleMap defaultZoom={10} center={{lat: lat, lng: lng}} >
             {renderMarkers()}
-            {selected && (
+            {props.selected && (
                 <InfoWindow 
-                    position={{lat: selected.geometry.location.lat, lng: selected.geometry.location.lng}}
-                    onCloseClick={() => props.handleAdd(null)}
+                    position={{lat: props.selected.geometry.location.lat, lng: props.selected.geometry.location.lng}}
+                    onCloseClick={() => props.handleSelectPlace(null)}
                     >
                     <div>
-                        <h2>{selected.name}</h2>
-                        <p>{selected.formatted_address}</p>
+                        <h2>{props.selected.name}</h2>
+                        <p>{props.selected.formatted_address}</p>
                     </div>
                 </InfoWindow>
             )}
