2fbafca374c77cdedd6289ed7ebae4c047457571
Christian Kastner <christianmkastner@gmail.com>
2020-04-28 13:11:35 -0700

    Updates

---
+++ precache-manifest.b8da651f68216f8120223c0b6207110a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a0e49258c112c0d99503db6c1385f0",
+    "url": "/kangaroo-hangout/index.html"
+  },
+  {
+    "revision": "af5c56d9986c52035243",
+    "url": "/kangaroo-hangout/static/css/2.4482b0e7.chunk.css"
+  },
+  {
+    "revision": "ed76bc90d14763f79228",
+    "url": "/kangaroo-hangout/static/css/main.d399f720.chunk.css"
+  },
+  {
+    "revision": "af5c56d9986c52035243",
+    "url": "/kangaroo-hangout/static/js/2.b5ca559e.chunk.js"
+  },
+  {
+    "revision": "a669bc843e9f5588147494f263861382",
+    "url": "/kangaroo-hangout/static/js/2.b5ca559e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed76bc90d14763f79228",
+    "url": "/kangaroo-hangout/static/js/main.768a3fa4.chunk.js"
+  },
+  {
+    "revision": "a69e60e9f4f8c2c4e464",
+    "url": "/kangaroo-hangout/static/js/runtime-main.f4b9943d.js"
+  },
+  {
+    "revision": "2b310f73a2938f018a78bb7b876f76b8",
+    "url": "/kangaroo-hangout/static/media/sand_dunes_sand_salt_ash_122212_1920x1080.2b310f73.jpg"
+  }
+]);
